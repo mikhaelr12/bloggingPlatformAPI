5bb40d4ca2756e2b22a502ae3379ed1131e8af95
Mihai Rusu <rusumiihai@gmail.com>
2024-10-05 00:24:33 +0300

    Initial commit

---
+++ src/test/java/md/practice/bloggingapi/BloggingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package md.practice.bloggingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
